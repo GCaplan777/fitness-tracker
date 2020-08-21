113e51ef96e05dc66d68c635412782c61f91ffda
GCaplan <gabriel.caplan@gmail.com>
2020-08-20 20:49:03 -0400

    created model, index.js in models folder

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
 // // routes
 // app.use(require("./routes/api.js"));
 
+// use html routes maybe refer to Starwars html
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
